1da0fba181cc7971444c2fb394f6863861b1665b
Roger Santos <roger17ribeiro09@gmail.com>
2026-02-12 19:02:49 -0300

    Adicionando a pasta components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Heading } from './components/Heading';
+
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
   return (
     <>
-      <h1> Roger Santos</h1>
+      <Heading />
       <p> A B C D E F G H I</p>
     </>
   );
