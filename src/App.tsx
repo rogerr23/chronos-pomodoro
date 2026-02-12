d19ea65b3a4896118b34ae5fb673daae8eb48ea9
Roger Santos <roger17ribeiro09@gmail.com>
2026-02-12 19:54:59 -0300

    Criando a estrutura HTML e ajeitando o tamanho e largura com o CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
-  console.log('Oi');
   return (
     <>
-      <Heading attr={123} attr2='String '>
-        Lebron James
-      </Heading>
-      <p> A B C D E F G H I</p>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FORM</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
