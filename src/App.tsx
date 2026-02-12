65c660ac6d099fd4586edcd6abd2faec34dcbc67
Roger Santos <roger17ribeiro09@gmail.com>
2026-02-12 19:22:10 -0300

    Adicionando cores, fontes e etc

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
+  console.log('Oi');
   return (
     <>
-      <Heading />
+      <Heading attr={123} attr2='String '>
+        Lebron James
+      </Heading>
       <p> A B C D E F G H I</p>
     </>
   );
