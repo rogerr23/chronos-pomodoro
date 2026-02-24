52908cb13ebdc5a23e1edfdb2fc203f2b5a4799b
Roger Santos <roger17ribeiro09@gmail.com>
2026-02-24 18:57:32 -0300

    Estilizando o component Logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,5 +1,13 @@
+import { TimerIcon } from 'lucide-react';
 import styles from './styles.module.css';
 
 export function Logo() {
-  return <div className={styles.logo}>LOGO</div>;
+  return (
+    <div className={styles.logo}>
+      <a className={styles.logoLink} href='#'>
+        <TimerIcon />
+        <span>Chronos</span>
+      </a>
+    </div>
+  );
 }
